33f5fc48685f9ffc9a7c79d551adb776ff04205e
chuksabadom <chuksabadom@gmail.com>
2025-08-08 23:26:44 +0200

    update url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/nerdy-resume-profile/',
+    base: '/emeka-cv/',
     plugins: [react()],
     build: {
         rollupOptions: {
